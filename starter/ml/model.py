a0278990ca8e7ba64bfb1ccf60336e91cfe85544
s.y <63178306+shawnyazdani@users.noreply.github.com>
2024-01-30 11:56:54 -0800

    Mods Made

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,7 +1,7 @@
-from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.metrics import fbeta_score, precision_score, recall_score, accuracy_score
+from sklearn.ensemble import AdaBoostClassifier
 
 
-# Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -12,18 +12,19 @@ def train_model(X_train, y_train):
         Training data.
     y_train : np.array
         Labels.
+
     Returns
     -------
     model
         Trained machine learning model.
     """
-
-    pass
-
+    clf = AdaBoostClassifier(random_state=42)
+    model = clf.fit(X_train, y_train)
+    return model
 
 def compute_model_metrics(y, preds):
     """
-    Validates the trained machine learning model using precision, recall, and F1.
+    Validates the trained machine learning model using precision, recall, F1, and accuracy.
 
     Inputs
     ------
@@ -36,11 +37,13 @@ def compute_model_metrics(y, preds):
     precision : float
     recall : float
     fbeta : float
+    accuracy : float
     """
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
     recall = recall_score(y, preds, zero_division=1)
-    return precision, recall, fbeta
+    accuracy = accuracy_score(y, preds)
+    return precision, recall, fbeta, accuracy
 
 
 def inference(model, X):
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,12 +1,18 @@
+import pandas as pd 
+import os
+import pickle
+from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics
+
 # Script to train machine learning model.
 
-from sklearn.model_selection import train_test_split
+data_path = os.path.join(os.getcwd(), '..',  'data' , 'census.csv')
+data = pd.read_csv(data_path)
+assert((data.isna().sum()  == 0).all() == True) #ensure no missing values
+data.rename(str.strip, axis='columns', inplace=True) #remove all white-spaces from column names
 
-# Add the necessary imports for the starter code.
-
-# Add code to load in the data.
-
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Using train-test split instead of  K-fold cross validation 
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -24,5 +30,16 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder = encoder, lb = lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
+Y_pred_train = model.predict(X_train)
+Y_pred_test = model.predict(X_test)
+precision_train, recall_train, fbeta_train, accuracy_train = compute_model_metrics(y_train, Y_pred_train)
+precision_test, recall_test, fbeta_test, accuracy_test = compute_model_metrics(y_test, Y_pred_test)
