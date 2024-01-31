03ccfebed1a97d453bcd42fe2e98b99c93c30023
s.y <63178306+shawnyazdani@users.noreply.github.com>
2024-01-30 21:03:09 -0800

    Data slicing implemented

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score, accuracy_score
 from sklearn.ensemble import AdaBoostClassifier
-
+from ml.data import process_data
 
 def train_model(X_train, y_train):
     """
@@ -45,6 +45,68 @@ def compute_model_metrics(y, preds):
     accuracy = accuracy_score(y, preds)
     return precision, recall, fbeta, accuracy
 
+def validate_data_slice_performance(data, model, feature, categorical_features, numerical_features, encoder, lb):
+    '''
+    Validates the performance of the trained ML model on dataset slices of interest.
+    
+    Inputs 
+    -----
+
+    '''
+    precisions = []
+    accuracies = []
+
+    #####Categorical Feature Data-Slice Validation
+    if feature in categorical_features:
+        feature_values = data[feature].value_counts().index.values
+        feature_value_frequencies = data[feature].value_counts().values
+        for feature_val, frequency in zip(feature_values, feature_value_frequencies):
+            #Perform data split assessment if there are more than 1000 occurences of the feature value
+            if frequency > 1000:
+                #Extracting data subset with given feature value for categorical var of interest.
+                data_subset = data[data[feature] == feature_val]
+                assert(len(data_subset) == frequency) #ensuring extracted data subset has the proper frequency
+                #Validating data slice performance
+                precision_set, recall_set, fbeta_set, accuracy_set = eval_data_slice(data_subset, model, categorical_features, encoder, lb)
+                print(f"Feature: {feature}, Value: {feature_val}")
+                print(f"Precision: {precision_set:.2f}, Recall: {recall_set:.2f}, F1-Score: {fbeta_set:.2f}, Accuracy: {accuracy_set:.2f} ")
+                precisions.append(precision_set); accuracies.append(accuracy_set)
+
+    #####Numerical Feature Data-Slice Validation
+    #Extracting medians for numerical feature, as this will inform our slices (> & < median value)
+    num_feature_values = data.describe().loc['50%'].index.values
+    num_feature_value_frequencies = data.describe().loc['50%'].values
+    feat_map = dict(zip(num_feature_values, num_feature_value_frequencies))
+
+    if feature in numerical_features:
+        #Subset of data where the numerical feature is less than or equal to its median
+        data_subset_1 = data[data[feature] <= feat_map[feature]] 
+        # Validating data slice performance with metrics.
+        precision_set, recall_set, fbeta_set, accuracy_set = eval_data_slice(data_subset_1, model, categorical_features, encoder, lb)
+        print(f"Feature: {feature}, Values <= {feat_map[feature]}")
+        print(f"Precision: {precision_set:.2f}, Recall: {recall_set:.2f}, F1-Score: {fbeta_set:.2f}, Accuracy: {accuracy_set:.2f} ")
+        precisions.append(precision_set); accuracies.append(accuracy_set)
+       
+        #Subset of data where the numerical feature is greater than its median
+        data_subset_2 = data[data[feature] > feat_map[feature]]
+        # Validating data slice performance with metrics.
+        precision_set, recall_set, fbeta_set, accuracy_set = eval_data_slice(data_subset_2, model, categorical_features, encoder, lb)
+        print(f"Feature: {feature}, Values > {feat_map[feature]}")
+        print(f"Precision: {precision_set:.2f}, Recall: {recall_set:.2f}, F1-Score: {fbeta_set:.2f}, Accuracy: {accuracy_set:.2f} ")
+        precisions.append(precision_set); accuracies.append(accuracy_set)
+    
+    return precisions, accuracies
+
+def eval_data_slice(data_subset, model, categorical_features, encoder, lb):
+    #splitting into features & values for data subset
+    X_set, y_set, _, _ = process_data(data_subset, categorical_features,
+                                            label="salary", training=False, encoder = encoder, lb = lb)       
+    #performing inference with model, using X_set
+    y_set_pred = inference(model, X_set)
+    #Validating performance with metrics.
+    precision_set, recall_set, fbeta_set, accuracy_set = compute_model_metrics(y_set, y_set_pred)
+    
+    return precision_set, recall_set, fbeta_set, accuracy_set
 
 def inference(model, X):
     """ Run model inferences and return the predictions.
@@ -63,3 +125,4 @@ def inference(model, X):
     
     preds = model.predict(X)
     return preds
+
--- starter/train_model.py
+++ starter/train_model.py
@@ -3,7 +3,7 @@ import os
 import pickle
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
-from ml.model import train_model, compute_model_metrics
+from ml.model import train_model, compute_model_metrics, inference
 
 # Script to train machine learning model.
 
@@ -11,12 +11,14 @@ data_path = os.path.join(os.getcwd(), '..',  'data' , 'census.csv')
 data = pd.read_csv(data_path)
 assert((data.isna().sum()  == 0).all() == True) #ensure no missing values
 data.rename(str.strip, axis='columns', inplace=True) #remove all white-spaces from column names
+data = data.map(lambda x: x.strip() if type(x) == str else x) #removing all leading/trailing whitespaces from text/categorical data.
 
 # Splitting data, using train-test split instead of  K-fold cross validation 
 train, test = train_test_split(data, test_size=0.20)
 
 #Pre-processing  for train & test splits.
-cat_features = [
+
+cat_features = [ #used for one-hot-encoding
     "workclass",
     "education",
     "marital-status",
@@ -31,7 +33,7 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 #Fitted One-hot encoder and label binarizer are applied to test set. 
-X_test, y_test, encoder, lb = process_data(
+X_test, y_test, _, _ = process_data(
     test, categorical_features=cat_features, label="salary", training=False, encoder = encoder, lb = lb
 )
 
@@ -41,10 +43,10 @@ with open('model.pkl', 'wb') as file:
     pickle.dump(model, file)
 
 #Making predictions using trained model
-Y_pred_train = model.predict(X_train)
-Y_pred_test = model.predict(X_test)
+Y_pred_train = inference(X_train)
+Y_pred_test = inference(X_test)
 
-#Assessing prediction metrics.
+#Assessing prediction metrics on both training & test set.
 precision_train, recall_train, fbeta_train, accuracy_train = compute_model_metrics(y_train, Y_pred_train)
 precision_test, recall_test, fbeta_test, accuracy_test = compute_model_metrics(y_test, Y_pred_test)
 print(f"Test Precision: {precision_test:.2f}, Test Recall: {recall_test:.2f},\
