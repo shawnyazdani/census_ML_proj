42b2880bf7530d5918d86ea0e54e9cacaf3476de
s.y <63178306+shawnyazdani@users.noreply.github.com>
2024-02-01 13:05:18 -0800

    adding choice b/w local and web running of inference request

--- perform_inference_request.py
+++ perform_inference_request.py
@@ -17,7 +17,16 @@ data = {"age": [42],
         "native-country":"United-States"
         }
 
-# response = requests.post("http://127.0.0.1:8000/inference/", data=json.dumps(data)) #Local-running version
-response = requests.post("https://census-inference-api.onrender.com/inference/", data=json.dumps(data)) #Web-running version, on Render
-prediction = response.json()
-print(f"Status Code: {response.status_code}, Infered Prediction: {prediction}, Note: 1 = Income > $50K, 0 = Income < $50K")
+app_local = 0
+if app_local == 1:
+        # Local running app versions:
+        response_get = requests.get("http://127.0.0.1:8000/")  #Local-running version, get request
+        response_post = requests.post("http://127.0.0.1:8000/inference/", data=json.dumps(data)) #Local-running version, post request
+else:
+        # Web-running app versions:
+        response_get = requests.get("https://census-inference-api.onrender.com/") #Web-running version on Render, get request
+        response_post = requests.post("https://census-inference-api.onrender.com/inference/", data=json.dumps(data)) #Web-running version on Render, post request
+
+prediction = response_post.json()
+print(f"GET Request, Status Code: {response_get.status_code}, Message: {response_get.json()}")
+print(f"POST Request, Status Code: {response_post.status_code}, Inferred Prediction: {prediction}, Note: 1 = Income > $50K, 0 = Income < $50K")
